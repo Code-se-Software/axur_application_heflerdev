641baed8c5cf912c92dd9ab217b10dc33fc392b7
HeflerDev <heflerdev@gmail.com>
2022-10-08 21:35:50 -0300

    AXUR - Adjust Pages

--- src/pages/Home/components/HomeForm.tsx
+++ src/pages/Home/components/HomeForm.tsx
@@ -1,28 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {searchService} from "../../../services/SearchService";
+import {MagnifyingGlass} from "../../../assets/icons/Icons";
+import {Button, Stack} from "react-bootstrap";
 
 const HomeForm = () => {
-    const [query, setQuery]: [query: string, setQuery: any] = useState("");
+    const [query, setQuery]: [query: string, setQuery: Dispatch<SetStateAction<string>>] = useState("");
+    const [submit, setSubmit]: [submit: string, setSubmit: Dispatch<SetStateAction<string>>] = useState("abcde");
+    const [errors, setErrors]: [errors: Array<string>, setErrors: Dispatch<SetStateAction<String[]>>] = useState([]);
 
     useEffect(() => {
-        searchService(query)
+        searchService(submit)
             .then(res => console.log(res))
-    }, [query])
+    }, [submit])
 
     function handleChange({target}: { target: any }) {
         setQuery(target.value)
     }
 
     function handleSubmit() {
-
+        if (query.length > 4) {
+            setSubmit(query);
+        }
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="query">
-                Query
-                <input type="text" onChange={handleChange}/>
-            </label>
+            <Stack gap={3}>
+                <div className="search-bar">
+                    <input type="text" onChange={handleChange} value={query}/>
+                    <div className="submit">
+                        <MagnifyingGlass size={28} color={"white"}/>
+                    </div>
+                </div>
+                <Button className="submit-button">
+                    <MagnifyingGlass size={28} color={"white"}/>
+                </Button>
+            </Stack>
         </form>
     )
 }
